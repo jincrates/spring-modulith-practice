97e9728538cc922a4fe901fb60c30b4e6e25f182
jincrates <94jingyu@gmail.com>
2025-01-04 10:06:29 +0900

    refactor: ApplicationModuleListener 처리로 변경

--- src/main/java/me/jincrates/modulith/product/Products.java
+++ src/main/java/me/jincrates/modulith/product/Products.java
@@ -2,8 +2,7 @@ package me.jincrates.modulith.product;
 
 import lombok.extern.slf4j.Slf4j;
 import me.jincrates.modulith.order.OrderPlacedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,14 +11,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class Products {
 
-    @Async  // async를 통해서 비동기로 처리할 수 있다.
-    @EventListener
+    //    @Async  // async를 통해서 비동기로 처리할 수 있다.
+//    @EventListener
+    @ApplicationModuleListener
+    // Async와 Transactional을 사용하고 있다.
     void receive(OrderPlacedEvent event) throws InterruptedException {
         log.info(
             "starting [{}]",
             event
         );
-        Thread.sleep(5_000);
+        Thread.sleep(10_000);
         log.info(
             "stopping [{}]",
             event
